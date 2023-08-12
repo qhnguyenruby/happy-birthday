df45f8ff35b25c5802df9cd5ffa64c82bc47c4f2
HuyNQ122 <60059654+qhnguyenruby@users.noreply.github.com>
2023-08-12 16:49:40 +0700

    Update js.js

--- js.js
+++ js.js
@@ -4,7 +4,7 @@ var w = (c.width = window.innerWidth),
   hw = w / 2, // half-width
   hh = h / 2,
   opts = {
-    strings: ["HAPPY BIRTHDAY!", "TO YOU"],
+    strings: ["HAPPY", "BIRTHDAY!", "TO YOU"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
